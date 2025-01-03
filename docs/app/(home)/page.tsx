2c77c598e355ef10d8435cd5215746f48e8273d7
Makisuo <31933546+Makisuo@users.noreply.github.com>
2025-01-03 17:54:12 +0100

    docs: add logo

--- docs/app/(home)/page.tsx
+++ docs/app/(home)/page.tsx
@@ -1,12 +1,13 @@
+import Image from "next/image"
 import Link from "next/link"
 
 export default function HomePage() {
 	return (
-		<main className="flex flex-1 flex-col justify-center text-center">
-			<h1 className="mb-4 text-2xl font-bold">PGLite Drizzle</h1>
+		<main className="flex flex-1 flex-col items-center justify-center space-y-6 text-center">
+			<Image src="/logo.svg" alt="PGLite Drizzle Logo" width={600} height={400} />
 			<p className="text-fd-muted-foreground">
 				You can open{" "}
-				<Link href="/docs" className="text-fd-foreground font-semibold underline">
+				<Link href="/docs" className="font-semibold text-fd-foreground underline">
 					/docs
 				</Link>{" "}
 				and see the documentation.
