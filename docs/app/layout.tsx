d938617edf0e890dc2c036d1fbc19d0029466421
Makisuo <31933546+Makisuo@users.noreply.github.com>
2025-01-03 18:12:47 +0100

    docs: add some seo

--- docs/app/layout.tsx
+++ docs/app/layout.tsx
@@ -1,11 +1,17 @@
 import "./global.css"
 import { RootProvider } from "fumadocs-ui/provider"
+import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import type { ReactNode } from "react"
 
 const inter = Inter({
 	subsets: ["latin"],
 })
+
+export const metadata: Metadata = {
+	title: "PGLite Drizzle",
+	description: "Enables you to use drizzle-orm together with PGLite and React",
+}
 
 export default function Layout({ children }: { children: ReactNode }) {
 	return (
